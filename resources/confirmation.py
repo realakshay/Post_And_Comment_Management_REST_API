bee595f821fd9ad82e71f2341cad1105fe3b3eed
realakshay <gaikwadakshay663@gmail.com>
2020-05-27 20:46:26 +0530

    Modify : Here new resource added for manually user activation or confirmation and see user confirmation

--- resources/confirmation.py
+++ resources/confirmation.py
@@ -1,7 +1,14 @@
+import traceback
+from time import time
+
 from flask_restful import Resource
+
+from libs.mailgun import MailGunException
 from models.confirmation import ConfirmationModel
+from models.user import UserModel
 from schemas.confirmation import ConfirmationSchema
 from libs.string import gettext
+
 confirmation_schema = ConfirmationSchema()
 
 
@@ -20,3 +27,46 @@ class Confirmation(Resource):
         confirmation.is_confirmed = True
         confirmation.insert_in_db()
         return {"Message": gettext("confirmation_successful")}
+
+
+class ConfirmationByUser(Resource):
+
+    @classmethod
+    def get(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"Message": gettext("confirmation_user_not_found").format(user_id)}
+        return (
+            {
+                "current time": int(time()),
+                "confirmation": [
+                    confirmation_schema.dump(each)
+                    for each in user.confirmation.order_by(ConfirmationModel.expired_at)
+                ],
+            },
+            200,
+        )
+
+    @classmethod
+    def post(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {"Message": gettext("confirmation_user_not_found").format(user_id)}, 401
+        try:
+            confirmation = user.most_recent_confirmation
+            if confirmation:
+                if confirmation.is_confirmed:
+                    return {"Message": gettext("confirmation_already_confirmed")}, 401
+                confirmation.force_to_expire()
+
+            new_confirmation = ConfirmationModel(user_id)
+            new_confirmation.insert_in_db()
+            user.send_confirmation_mail()
+            return {"Message": gettext("confirmation_mail_resend_successful")}, 201
+
+        except MailGunException as me:
+            return {"Message": str(me)}, 501
+
+        except:
+            traceback.print_exc()
+            return {"Message": gettext("confirmation_mail_resend_failed")}, 401
