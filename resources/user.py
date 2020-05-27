5586230fca53abd78a7f99fc98b5454f339dce38
realakshay <gaikwadakshay663@gmail.com>
2020-05-27 21:05:21 +0530

    Modify : Here resource for user login has created with encrypted password

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,9 @@ import traceback
 
 from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import create_refresh_token, create_access_token, jwt_required
 from marshmallow import ValidationError
+from werkzeug.security import safe_str_cmp, generate_password_hash, check_password_hash
 
 from libs.mailgun import MailGunException
 from models.confirmation import ConfirmationModel
@@ -20,6 +22,8 @@ class UserRegister(Resource):
         user_json = request.get_json()
         try:
             user_data = user_schema.load(user_json)
+            user_data.password = generate_password_hash(user_data.password)
+            print(user_data.password)
         except ValidationError as err:
             return err.messages, 401
 
@@ -61,3 +65,24 @@ class User(Resource):
             return {"Message": gettext("user_id_not_found_error").format(user_id)}, 401
         user.delete_from_db()
         return {"Message": gettext("user_deleted_successful").format(user_id)}, 201
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        user_json = request.get_json()
+        try:
+            user_data = user_schema.dump(user_json)
+        except ValidationError as err:
+            return {"Message": err.messages}, 401
+
+        user = UserModel.find_by_username(user_data.username)
+        if user and check_password_hash(user.password, user_data.password):
+            confirmation = user.most_recent_confirmation
+            if confirmation and confirmation.is_confirmed:
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(identity=user.id)
+                return {"access_token": access_token, "refresh_token":refresh_token}
+            return {"Message": gettext("user_not_confirmed_error")}
+        return {"Message": gettext("user_invalid_credential_error")}
